84ade650853afe0ba9cdbdec34075cd2eaab8ec6
Pedro Braz <pedrobraz2002@gmail.com>
2022-12-04 18:06:21 +0000

    Implemented Historical

--- server_api/app/Http/Controllers/api/CustomerController.php
+++ server_api/app/Http/Controllers/api/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderResource;
 
 class CustomerController extends Controller
 {
@@ -14,5 +15,13 @@ class CustomerController extends Controller
 
     public function getCustomerReference(Customer $customer){
         return $customer->default_payment_reference;
+    }
+
+    public function showOrders(Request $request, Customer $customer)
+    {
+        //return $customer->order;
+        return OrderResource::collection($customer->order);
+        //return response()->json($customer->order);
+
     }
 }
